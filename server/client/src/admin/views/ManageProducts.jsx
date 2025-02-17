06ed175211c95b72af2582c6d3185155e00721ca
zahid0033 <zahidhossain0033@gmail.com>
2025-02-17 17:57:30 +0600

    Changed the image link from the local directory to remote url

--- server/client/src/admin/views/ManageProducts.jsx
+++ server/client/src/admin/views/ManageProducts.jsx
@@ -1,184 +1,239 @@
-import React,{Component} from "react";
-import axios from 'axios';
-import PanelHeader from "../components/PanelHeader/PanelHeader";
-import {Card, CardBody, Table} from "reactstrap";
+import React, { Component } from 'react'
+import axios from 'axios'
+import PanelHeader from '../components/PanelHeader/PanelHeader'
+import { Card, CardBody, Table } from 'reactstrap'
 
-import {tbody, thead} from "../variables/general";
-import {Link} from "react-router-dom";
-import authHeader from "../adminServices/authHeader";
+import { tbody, thead } from '../variables/general'
+import { Link } from 'react-router-dom'
+import authHeader from '../adminServices/authHeader'
 //sweetalert2
 import Swal from 'sweetalert2/dist/sweetalert2.js'
 import 'sweetalert2/src/sweetalert2.scss'
 
-class ManageProducts extends Component{
-    state = {
-        products : [],
-        exclusive : "",
-        featured : ""
-    };
+class ManageProducts extends Component {
+  state = {
+    products: [],
+    exclusive: '',
+    featured: '',
+  }
 
-    componentDidMount() {
-        this.loadProducts();
+  componentDidMount() {
+    this.loadProducts()
+  }
+
+  loadProducts = async () => {
+    await axios.get(`/api/product`).then((res) => {
+      this.setState({
+        products: res.data.output,
+      })
+    })
+  }
+
+  exclusiveHandle = async (status, id) => {
+    if (status === 'no') {
+      await this.setState({
+        exclusive: 'yes',
+      })
+    } else if (status === 'yes') {
+      await this.setState({
+        exclusive: 'no',
+      })
     }
 
-    loadProducts = async () => {
-        await axios.get(`/api/product`)
-            .then(res => {
-                this.setState({
-                    products : res.data.output
-                })
-            })
-    };
+    await axios
+      .post(
+        `/api/product/exclusiveUpdate`,
+        { status: this.state.exclusive, id: id },
+        { headers: { 'x-access-token': authHeader() } }
+      )
+      .then((res) => {
+        this.props.handleNotification('tr', 'Exclusive Successfully Updated')
+        this.loadProducts()
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }
 
-    exclusiveHandle = async (status,id) => {
-        if (status === "no"){
-            await this.setState({
-                exclusive : "yes"
-            });
-        }else if(status === "yes"){
-            await this.setState({
-                exclusive : "no"
-            });
+  featuredHandle = async (status, id) => {
+    if (status === 'no') {
+      await this.setState({
+        featured: 'yes',
+      })
+    } else if (status === 'yes') {
+      await this.setState({
+        featured: 'no',
+      })
+    }
+
+    await axios
+      .post(
+        `/api/product/featuredUpdate`,
+        { status: this.state.featured, id: id },
+        { headers: { 'x-access-token': authHeader() } }
+      )
+      .then((res) => {
+        this.props.handleNotification('tr', 'Featured Successfully Updated')
+        this.loadProducts()
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }
+
+  onDelete = (id) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it',
+    }).then((result) => {
+      if (result.value) {
+        this.sendDelete(id)
+        // For more information about handling dismissals please visit
+        // https://sweetalert2.github.io/#handling-dismissals
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Your imaginary file is safe :)', 'error')
+      }
+    })
+  }
+
+  sendDelete = async (id) => {
+    await axios
+      .delete(`/api/product/delete/${id}`)
+      .then((response) => {
+        if (response.data.success) {
+          Swal.fire('Deleted!', 'Product has been deleted.', 'success')
+          this.loadProducts()
         }
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }
 
-        await axios.post(`/api/product/exclusiveUpdate`,{status : this.state.exclusive,id: id},{headers : {"x-access-token" : authHeader()}})
-            .then(res =>{
-                this.props.handleNotification("tr","Exclusive Successfully Updated");
-                this.loadProducts();
-            })
-            .catch(error => {
-                alert(error)
-            })
-
-    };
-
-    featuredHandle = async (status,id) => {
-        if (status === "no"){
-            await this.setState({
-                featured : "yes"
-            });
-        }else if(status === "yes"){
-            await this.setState({
-                featured : "no"
-            });
-        }
-
-        await axios.post(`/api/product/featuredUpdate`,{status : this.state.featured,id: id},{headers : {"x-access-token" : authHeader()}})
-            .then(res =>{
-                this.props.handleNotification("tr","Featured Successfully Updated");
-                this.loadProducts();
-            })
-            .catch(error => {
-                alert(error)
-            })
-
-    };
-
-    onDelete = (id) => {
-        Swal.fire({
-            title: 'Are you sure?',
-            text: 'You will not be able to recover this file!',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonText: 'Yes, delete it!',
-            cancelButtonText: 'No, keep it'
-        }).then((result) => {
-            if (result.value) {
-                this.sendDelete(id)
-                // For more information about handling dismissals please visit
-                // https://sweetalert2.github.io/#handling-dismissals
-            } else if (result.dismiss === Swal.DismissReason.cancel) {
-                Swal.fire(
-                    'Cancelled',
-                    'Your imaginary file is safe :)',
-                    'error'
-                )
-            }
-        })
-    };
-
-    sendDelete = async (id) =>{
-        await axios.delete(`/api/product/delete/${id}`)
-            .then(response => {
-                if (response.data.success) {
-                    Swal.fire(
-                        'Deleted!',
-                        'Product has been deleted.',
-                        'success'
-                    );
-                    this.loadProducts();
+  fillProductsData = () => {
+    return this.state.products.map((product, key) => {
+      return (
+        <tr key={key}>
+          <th>{key + 1}</th>
+          <td>{product.name}</td>
+          <td>{product.price}</td>
+          <td>
+            {product.exclusive === 'no' ? (
+              <span
+                className='btn btn-danger'
+                onClick={() =>
+                  this.exclusiveHandle(product.exclusive, product.id)
                 }
-            })
-            .catch(error => {
-                alert(error)
-            })
-    };
+              >
+                <i className='fas fa-tags' />
+              </span>
+            ) : (
+              <span
+                className='btn btn-success'
+                onClick={() =>
+                  this.exclusiveHandle(product.exclusive, product.id)
+                }
+              >
+                <i className='fas fa-tags' />
+              </span>
+            )}
+          </td>
+          <td>
+            {product.featured === 'no' ? (
+              <span
+                className='btn btn-danger'
+                onClick={() =>
+                  this.featuredHandle(product.featured, product.id)
+                }
+              >
+                <i className='far fa-star' />
+              </span>
+            ) : (
+              <span
+                className='btn btn-success'
+                onClick={() =>
+                  this.featuredHandle(product.featured, product.id)
+                }
+              >
+                <i className='far fa-star' />
+              </span>
+            )}
+          </td>
+          {/*<td>action</td>*/}
+          <td>
+            <span className='mr-2'>{product.supplier.name}</span>
+            <span
+              className={
+                product.supplier.status === 'Platinum'
+                  ? 'badge badge-success'
+                  : product.supplier.status === 'Gold'
+                  ? 'badge badge-warning'
+                  : product.supplier.status === 'Verified'
+                  ? 'badge badge-danger'
+                  : product.supplier.status === 'Silver'
+                  ? 'badge badge-primary'
+                  : 'badge badge-dark'
+              }
+            >
+              {' '}
+              {product.supplier.status}{' '}
+            </span>
+          </td>
+          <td>{product.subCategory.name}</td>
+          <td>
+            <Link
+              className='btn btn-info mr-2'
+              to={'/admin/viewProductDetails/' + product.id}
+            >
+              View
+            </Link>
+            <span
+              className='btn btn-danger'
+              onClick={() => this.onDelete(product.id)}
+            >
+              <i className='now-ui-icons ui-1_simple-remove'></i>
+            </span>
+          </td>
+          {/*<td>*/}
+          {/*    <button className="btn btn-outline-danger" onClick={()=>{this.onDelete(admin.id)}}> Delete</button>*/}
+          {/*</td>*/}
+        </tr>
+      )
+    })
+  }
 
-    fillProductsData = () => {
-        return this.state.products.map((product,key) => {
-            return (
-                <tr key={key}>
-                    <th>{key+1}</th>
-                    <td>{product.name}</td>
-                    <td>{product.price}</td>
-                    <td>{product.exclusive === 'no' ? <span className="btn btn-danger" onClick={ () => this.exclusiveHandle(product.exclusive ,product.id)}><i className="fas fa-tags"/></span> : <span className="btn btn-success" onClick={ () => this.exclusiveHandle(product.exclusive ,product.id)}><i className="fas fa-tags"/></span>}</td>
-                    <td>{product.featured === 'no' ? <span className="btn btn-danger"  onClick={ () => this.featuredHandle(product.featured ,product.id)}><i className="far fa-star"/></span> : <span className="btn btn-success"  onClick={ () => this.featuredHandle(product.featured, product.id)}><i className="far fa-star"/></span>}</td>
-                    {/*<td>action</td>*/}
-                    <td>
-                        <span className="mr-2">{product.supplier.name}</span>
-                        <span
-                            className = {
-                                product.supplier.status === "Platinum" ? "badge badge-success"
-                                    : product.supplier.status === "Gold" ? "badge badge-warning"
-                                    : product.supplier.status === "Verified" ? "badge badge-danger"
-                                        : product.supplier.status === "Silver" ? "badge badge-primary"
-                                            : "badge badge-dark"
-                            }
-                        > {product.supplier.status} </span>
-                    </td>
-                    <td>{product.subCategory.name}</td>
-                    <td>
-                        <Link className="btn btn-info mr-2" to={"/admin/viewProductDetails/"+product.id}>View</Link>
-                        <span className="btn btn-danger" onClick={()=> this.onDelete(product.id)}><i className="now-ui-icons ui-1_simple-remove"></i></span>
-                    </td>
-                    {/*<td>*/}
-                    {/*    <button className="btn btn-outline-danger" onClick={()=>{this.onDelete(admin.id)}}> Delete</button>*/}
-                    {/*</td>*/}
-                </tr>
-            )
-        })
-    };
-
-    render() {
-        return (
-            <>
-                <PanelHeader size="sm" />
-                <div className="content">
-                    <Card>
-                        <CardBody>
-                            <Table responsive>
-                                <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Price</th>
-                                    <th scope="col">Exclusive</th>
-                                    <th scope="col">Featured</th>
-                                    <th scope="col">Supplier</th>
-                                    <th scope="col">Category</th>
-                                    <th scope="col">Action</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {this.fillProductsData()}
-                                </tbody>
-                            </Table>
-                        </CardBody>
-                    </Card>
-                </div>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <PanelHeader size='sm' />
+        <div className='content'>
+          <Card>
+            <CardBody>
+              <Table responsive>
+                <thead>
+                  <tr>
+                    <th scope='col'>#</th>
+                    <th scope='col'>Name</th>
+                    <th scope='col'>Price</th>
+                    <th scope='col'>Exclusive</th>
+                    <th scope='col'>Featured</th>
+                    <th scope='col'>Supplier</th>
+                    <th scope='col'>Category</th>
+                    <th scope='col'>Action</th>
+                  </tr>
+                </thead>
+                <tbody>{this.fillProductsData()}</tbody>
+              </Table>
+            </CardBody>
+          </Card>
+        </div>
+      </>
+    )
+  }
 }
 
 export default ManageProducts
--- server/client/src/user/views/forgetPassword.jsx
+++ server/client/src/user/views/forgetPassword.jsx
@@ -1,138 +1,149 @@
-import React,{Component} from "react";
-import axios from 'axios';
-import Breadcrumbs from "../widgets/Breadcrumbs/breadcrumbs";
-import {Link} from "react-router-dom";
+import React, { Component } from 'react'
+import axios from 'axios'
+import Breadcrumbs from '../widgets/Breadcrumbs/breadcrumbs'
+import { Link } from 'react-router-dom'
 // sweetalert2
 import Swal from 'sweetalert2/dist/sweetalert2.js'
 import 'sweetalert2/src/sweetalert2.scss'
 //validation
-import SimpleReactValidator from 'simple-react-validator';
+import SimpleReactValidator from 'simple-react-validator'
 
-
-class ForgetPassword extends Component{
-    constructor() {
-        super();
-        this.validator = new SimpleReactValidator({autoForceUpdate: this});
-        this.state = {
-            email: "",
-            message : null,
-            sending : false
-        };
+class ForgetPassword extends Component {
+  constructor() {
+    super()
+    this.validator = new SimpleReactValidator({ autoForceUpdate: this })
+    this.state = {
+      email: '',
+      message: null,
+      sending: false,
     }
-    handleChange = event => {
-        this.validator.showMessages();
-        this.setState({
-            [event.target.name] : event.target.value
-        })
-    };
+  }
+  handleChange = (event) => {
+    this.validator.showMessages()
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
 
-    onSave = async (e) => {
-        e.preventDefault();
-        if (this.validator.allValid()){
-            const dataPost = {
-                email : this.state.email
-            };
+  onSave = async (e) => {
+    e.preventDefault()
+    if (this.validator.allValid()) {
+      const dataPost = {
+        email: this.state.email,
+      }
+      this.setState({
+        sending: true,
+      })
+
+      await axios
+        .post(`/api/supplier/forgetPassword`, dataPost)
+        .then((response) => {
+          if (response.data.success === true) {
             this.setState({
-                sending : true
-            });
-
-            await axios.post(`/api/supplier/forgetPassword`,dataPost)
-                .then(response => {
-                    if (response.data.success === true) {
-                        this.setState({
-                            email: '',
-                            message: response.data.message,
-                            sending : false
-                        })
-                    }
-                    else {
-                        alert("hey " + response.data.message)
-                    }
-                })
-                .catch(error=>{
-                    if (error.response.data.success === false){
-                        this.setState({
-                            sending : false,
-                            message: error.response.data.message
-                        })
-                    }
-                    else if(error.response.data.name === "server error"){
-                        this.setState({
-                            sending : false,
-                            errorMessage : error.response.data.error[0].message
-                        })
-                    }
-                });
-        }
-        else{
-            this.validator.showMessages();
-        }
-
-    };
-
-    render() {
-        return (
-            <>
-                {/* START SECTION BREADCRUMB */}
-                <Breadcrumbs title="Forget Password"/>
-                {/* END SECTION BREADCRUMB */}
-                <div className="main_content">
-                    {/* START LOGIN SECTION */}
-                    <div className="login_register_wrap section">
-                        <div className="container">
-                            <div className="row justify-content-center">
-                                <div className="col-xl-6 col-md-10">
-                                    <div className="login_wrap">
-                                        <div className="padding_eight_all bg-white">
-                                            <div className="heading_s1">
-                                                {this.state.message && <p className="alert alert-primary">{this.state.message}</p>}
-                                                {this.state.sending ? <p className="alert alert-success">Sending Please Wait.....</p> : ''}
-                                                <h3>Forget Password</h3>
-                                            </div>
-                                            <form>
-                                                <div className="form-group">
-                                                    <input
-                                                        type="text"
-                                                        required
-                                                        className="form-control"
-                                                        name="email"
-                                                        placeholder="Your Email"
-                                                        value={this.state.email}
-                                                        onChange={this.handleChange}
-                                                    />
-                                                    {this.validator.message('email', this.state.email, 'required|email',{ className: 'text-danger' })}
-                                                </div>
-
-                                                <div className="form-group">
-                                                    <button
-                                                        type="submit"
-                                                        className="btn btn-fill-out btn-block"
-                                                        name="login"
-                                                        onClick={this.onSave}
-                                                    >
-                                                        Reset Password
-                                                    </button>
-                                                </div>
-                                            </form>
-                                            <div className="different_login">
-                                                <span> or</span>
-                                            </div>
-
-                                            <div className="form-note text-center">
-                                                Remember Password? <Link to={"/supplier/signin"}>Back to Signin</Link>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* END LOGIN SECTION */}
-                </div>
-
-            </>
-        );
+              email: '',
+              message: response.data.message,
+              sending: false,
+            })
+          } else {
+            alert('hey ' + response.data.message)
+          }
+        })
+        .catch((error) => {
+          if (error.response.data.success === false) {
+            this.setState({
+              sending: false,
+              message: error.response.data.message,
+            })
+          } else if (error.response.data.name === 'server error') {
+            this.setState({
+              sending: false,
+              errorMessage: error.response.data.error[0].message,
+            })
+          }
+        })
+    } else {
+      this.validator.showMessages()
     }
+  }
+
+  render() {
+    return (
+      <>
+        {/* START SECTION BREADCRUMB */}
+        <Breadcrumbs title='Forget Password' />
+        {/* END SECTION BREADCRUMB */}
+        <div className='main_content'>
+          {/* START LOGIN SECTION */}
+          <div className='login_register_wrap section'>
+            <div className='container'>
+              <div className='row justify-content-center'>
+                <div className='col-xl-6 col-md-10'>
+                  <div className='login_wrap'>
+                    <div className='padding_eight_all bg-white'>
+                      <div className='heading_s1'>
+                        {this.state.message && (
+                          <p className='alert alert-primary'>
+                            {this.state.message}
+                          </p>
+                        )}
+                        {this.state.sending ? (
+                          <p className='alert alert-success'>
+                            Sending Please Wait.....
+                          </p>
+                        ) : (
+                          ''
+                        )}
+                        <h3>Forget Password</h3>
+                      </div>
+                      <form>
+                        <div className='form-group'>
+                          <input
+                            type='text'
+                            required
+                            className='form-control'
+                            name='email'
+                            placeholder='Your Email'
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                          />
+                          {this.validator.message(
+                            'email',
+                            this.state.email,
+                            'required|email',
+                            { className: 'text-danger' }
+                          )}
+                        </div>
+
+                        <div className='form-group'>
+                          <button
+                            type='submit'
+                            className='btn btn-fill-out btn-block'
+                            name='login'
+                            onClick={this.onSave}
+                          >
+                            Reset Password
+                          </button>
+                        </div>
+                      </form>
+                      <div className='different_login'>
+                        <span> or</span>
+                      </div>
+
+                      <div className='form-note text-center'>
+                        Remember Password?{' '}
+                        <Link to={'/supplier/signin'}>Back to Signin</Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* END LOGIN SECTION */}
+        </div>
+      </>
+    )
+  }
 }
 
 export default ForgetPassword
